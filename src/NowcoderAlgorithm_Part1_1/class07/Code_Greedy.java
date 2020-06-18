64704570e0051e632277c666bbdfa954b1221100
liyan20 <xianzhengwu@126.com>
2020-06-18 09:04:45 +0800

    0618提交

--- src/NowcoderAlgorithm_Part1_1/class07/Code_Greedy.java
+++ src/NowcoderAlgorithm_Part1_1/class07/Code_Greedy.java
@@ -19,10 +19,82 @@ package NowcoderAlgorithm_Part1_1.class07;
  *
  * 总的来说，你觉得这种方法可行，干就完了。比如会议室开会，会议有开始时间和结束时间，让你选怎么让开的会最多。
  * 有几种想法，一个是按照开始时间安排；按照持续时间安排；按照结束时间安排
- * 正确答案是结束时间安排，可以整个对数器
+ * 正确答案是按照结束时间安排，可以整个对数器
+ *
+ * 对数器：给定最大数组长度，最大值，使用Math.random()方法，获得小于这个最大值的随机数，获得随机的数值
+ * 就可以得到一个随机的数组了
  * @Analysis:
  * @Summary:
  * @TimeConsuming:
  */
 public class Code_Greedy {
+    //生成随机数组，随机有限长度和随机有限最大值的数组
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test复制数组，用于可能使得数组内容变化的情况
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test判断数组是否相等
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //我们的方法和绝对正确的方法，比如纯粹暴力求可能的排列组合
+    public static void solution1(int[] arr){
+    }
+    public static void solution2(int[] arr){
+    }
+    public static void printArray(int[] arr){
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            //处理两个数组
+            solution1(arr1);
+            solution2(arr2);
+            //如果两个方法的结果不一样，打印出来哪里不一样的
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
 }
